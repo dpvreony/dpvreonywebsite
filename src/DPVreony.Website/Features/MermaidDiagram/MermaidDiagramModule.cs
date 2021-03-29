d2577fdbfb8753f14adf2cb80a7c02cdb508f859
David Vreony <dpvreony@users.noreply.github.com>
2021-03-29 14:03:45 +0100

    Mermaidpipeline (#25)
    * initial shellexecute class for mermaid
    
    * mmd file generation via module

---
+++ src/DPVreony.Website/Features/MermaidDiagram/MermaidDiagramModule.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Statiq.Common;
+
+namespace DPVreony.Website.Features.MermaidDiagram
+{
+    public sealed class MermaidDiagramModule : Module
+    {
+        /// <inheritdoc />
+        protected override async Task<IEnumerable<IDocument>> ExecuteInputAsync(IDocument input, IExecutionContext context)
+        {
+            context.LogInformation(input, "Starting ShellExecute Module");
+
+            var inputFilename = Path.GetFullPath(input.Source.FullPath);
+
+            var rootPath = Path.GetFullPath(context.FileSystem.RootPath.FullPath);
+
+            var destination = input.Destination.FullPath;
+            //var destinationAsPath = Path.GetPathRoot(destination);
+
+
+            var outputFilename = Path.Combine(
+                rootPath,
+                "output",
+                destination);
+
+            outputFilename = Path.GetFullPath(outputFilename);
+
+            outputFilename = Path.ChangeExtension(
+                outputFilename,
+                ".svg");
+
+            var targetDir = Path.GetDirectoryName(outputFilename);
+
+            if (!Directory.Exists(targetDir))
+            {
+                Directory.CreateDirectory(targetDir);
+            }
+
+            var command = Path.Combine(
+                rootPath,
+                "node_modules\\.bin\\mmdc.cmd");
+
+            var args = $"-i \"{inputFilename}\" -o \"{outputFilename}\"";
+
+            context.LogInformation(input, $"Input Filename: {inputFilename}");
+            context.LogInformation(input, $"Output Filename: {outputFilename}");
+            context.LogInformation(input, $"Command: {command}");
+            context.LogInformation(input, $"Args: {args}");
+
+            if (!File.Exists(command))
+            {
+                throw new FileNotFoundException(".mmd files require NPM and mermaid-cli", command);
+            }
+
+            var startInfo = new ProcessStartInfo(command, args);
+            startInfo.WorkingDirectory = rootPath;
+
+            var process = Process.Start(startInfo);
+            process.WaitForExit();
+
+            var exitCode = process.ExitCode;
+            if (exitCode != 0)
+            {
+                throw new InvalidOperationException($"Process exit code is: {exitCode}");
+            }
+
+            return Array.Empty<IDocument>();
+        }
+    }
+}
---
+++ src/DPVreony.Website/Features/MermaidDiagram/MermaidDiagramPipeline.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DPVreony.Website.Features.MermaidDiagram;
+using Statiq.Common;
+using Statiq.Core;
+
+namespace DPVreony.Website.Pipelines
+{
+    public sealed class MermaidDiagramPipeline : Pipeline
+    {
+        public MermaidDiagramPipeline()
+        {
+            // we need to check for mermaidjs-cli
+            // we need to look for *.mmd files
+            this.InputModules = new ModuleList(new ReadFiles("./**/*.mmd"));
+
+            this.ProcessModules = new ModuleList(new MermaidDiagramModule());
+        }
+    }
+}
