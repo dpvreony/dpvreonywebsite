1873fa6a5966de328d6ab862861954b9ec3a7672
David Vreony <dpvreony@users.noreply.github.com>
2021-04-18 16:26:24 +0100

    #29 #30 work on article generation

---
+++ src/DPVreony.Website/Features/Articles/ArticleHelpers.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DPVreony.Website.Features.Articles
+{
+    class ArticleHelpers
+    {
+    }
+}
---
+++ src/DPVreony.Website/Features/Articles/ArticleMetadataModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DPVreony.Website.Features.Articles
+{
+    public sealed class ArticleMetadataModel
+    {
+        public ArticleMetadataModel(
+            ArticleStatus status,
+            Version version,
+            NodaTime.LocalDate firstRevision,
+            NodaTime.LocalDate lastRevision,
+            NodaTime.LocalDate lastReview,
+            NodaTime.LocalDate nextReview)
+        {
+            Status = status;
+            Version = version;
+            FirstRevision = firstRevision;
+            LastRevision = lastRevision;
+            LastReview = lastReview;
+            NextReview = nextReview;
+        }
+
+        public ArticleStatus Status { get; }
+
+        public Version Version { get; }
+
+        public NodaTime.LocalDate FirstRevision { get; }
+
+        public NodaTime.LocalDate LastRevision { get; }
+
+        public NodaTime.LocalDate LastReview { get; }
+
+        public NodaTime.LocalDate NextReview { get; }
+    }
+}
