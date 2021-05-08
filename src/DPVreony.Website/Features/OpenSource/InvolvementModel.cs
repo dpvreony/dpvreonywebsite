1846240d17e18d67ceacfc2d43f02ea66d3c142c
David Vreony <dpvreony@users.noreply.github.com>
2021-05-08 19:06:19 +0100

    model for open source involvement items

---
+++ src/DPVreony.Website/Features/OpenSource/InvolvementModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DPVreony.Website.Features.OpenSource
+{
+    public sealed class InvolvementModel
+    {
+        public InvolvementModel(
+            string title,
+            string description,
+            Uri githubUri,
+            Uri articleUri)
+        {
+            Title = title;
+            Description = description;
+            GithubUri = githubUri;
+            ArticleUri = articleUri;
+        }
+
+        public string Title { get; }
+
+        public string Description { get; }
+
+        public Uri GithubUri { get; }
+
+        public Uri ArticleUri { get; }
+    }
+}
