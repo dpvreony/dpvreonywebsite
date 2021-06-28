2c100fb7cf85a2aac0eecd0d46ba02bbe6dee0a6
David Vreony <dpvreony@users.noreply.github.com>
2021-06-28 21:32:23 +0100

    Securityheaders (#68)
    * add security headers
    
    * http header permissions policy
    
    * npm update
    
    * bump statiq
    
    * pipeline for netlify config files

---
+++ src/DPVreony.Website/Features/NetlifyConfigFiles/NetlifyConfigFilesPipeline.cs
@@ -0,0 +1,14 @@
+﻿using Statiq.Common;
+using Statiq.Core;
+
+namespace DPVreony.Website.Features.NetlifyConfigFiles
+{
+    public sealed class NetlifyConfigFilesPipeline : Pipeline
+    {
+        public NetlifyConfigFilesPipeline()
+        {
+            this.InputModules = new ModuleList(new ReadFiles("{_headers,netlify.toml,_redirects}"));
+            this.OutputModules = new ModuleList(new WriteFiles());
+        }
+    }
+}
