45c8c0e6fec22c57f2c8c8d43a8fdbd7f62cba78
dpvreony <dpvreony@users.noreply.github.com>
2022-12-19 18:20:34 +0000

    initial piece of lighthouse thoughts

---
+++ src/DPVreony.IntegrationTests/LighthouseTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPVreony.IntegrationTests
+{
+    public class LighthouseTests
+    {
+        public async Task RunsLighthouse()
+        {
+            // we need to run statiq build
+            // then start the test server
+            // then run lighthouse
+        }
+    }
+}
