942d3dd10b1dbb278e033aec47c5875ba3578632
David Vreony <dpvreony@users.noreply.github.com>
2026-01-24 00:19:53 +0000

    add aspire and aspnetstatic

---
+++ src/DPVreony.Website.AspireAppHost/Program.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) 2021 - 2024 DHGMS Solutions and Contributors. All rights reserved.
+// DHGMS Solutions and Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using Aspire.Hosting;
+using Aspire.Hosting.ApplicationModel;
+using Whipstaff.Aspire.Hosting;
+using Whipstaff.Aspire.Hosting.HealthChecksUI;
+using Whipstaff.Aspire.Hosting.ZedAttackProxy;
+
+namespace VitaeCyclum.AspireAppHost
+{
+    /// <summary>
+    /// Hosts the application.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Main entry point for the application.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            var builder = GetDistributedApplicationBuilder(args);
+
+            var app = builder.Build();
+            app.Run();
+        }
+
+        private static IDistributedApplicationBuilder GetDistributedApplicationBuilder(string[] args)
+        {
+            var builder = DistributedApplication.CreateBuilder(args);
+
+            //GetProjects(builder);
+
+            return builder;
+        }
+
+#if TBC
+        private static IResourceBuilder<ProjectResource> GetProjects(IDistributedApplicationBuilder builder)
+        {
+            return builder.AddProject<Projects.VitaeCyclum_PublicWebsite>("public-website");
+        }
+#endif
+    }
+}
---
+++ src/DPVreony.WebsiteBuilder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using AspNetStatic;
+using AspNetStaticContrib.AspNetStatic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DPVreony.WebsiteBuilder
+{
+    public static class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddSingleton<IStaticResourcesInfoProvider>(
+                new StaticResourcesInfoProvider()
+                    .AddAllProjectRazorPages(builder.Environment)
+                    .AddAllWebRootContent(builder.Environment));
+
+            // TODO: add index page
+            // TODO: add the .mmd file conversion
+
+            var app = builder.Build();
+            app.MapRazorPages();
+#if TBC
+            app.GenerateStaticContent("../../../../DPVreony.Website/wwwroot");
+#endif
+            app.Run();
+        }
+    }
+}
