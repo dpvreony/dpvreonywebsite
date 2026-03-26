43a9dbc465d1198956e3c8cc16d6471f6887dc26
David Vreony <dpvreony@users.noreply.github.com>
2026-03-26 15:42:41 +0000

    more concepts around staticdev

--- src/DPVreony.WebsiteBuilder/Program.cs
+++ src/DPVreony.WebsiteBuilder/Program.cs
@@ -1,8 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿// #define ENABLE_STATIC_PAGE_FALLBACK
+
+using System;
+using System.Threading.Tasks;
 using AspNetStatic;
 using AspNetStaticContrib.AspNetStatic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 
 namespace DPVreony.WebsiteBuilder
 {
@@ -10,24 +15,122 @@ namespace DPVreony.WebsiteBuilder
     {
         public static async Task<int> Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddSingleton<IStaticResourcesInfoProvider>(
-                new StaticResourcesInfoProvider()
-                    .AddAllProjectRazorPages(builder.Environment)
-                    .AddAllWebRootContent(builder.Environment));
 
-            // TODO: add index page
-            // TODO: add the .mmd file conversion
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddRouting(
+                options =>
+                {
+                    options.LowercaseUrls = true;
+                    options.LowercaseQueryStrings = true;
+                    options.AppendTrailingSlash = false;
+                });
+
+            builder.Services.AddRazorPages();
+
+            builder.Services.AddSingleton<IStaticResourcesInfoProvider>(
+                GetStaticResourcesInfoProvider(builder)
+                );
+
+            // Use the "no-ssg" arg to omit static file generation
+            // during development (hot-reload, etc.)
+            var allowSSG = !args.HasOmitSsgArg();
+
+            var exitWhenDone = args.HasExitWhenDoneArg();
+
+            TimeSpan? regenInterval =
+                !exitWhenDone &&
+                TimeSpan.TryParse(builder.Configuration["AspNetStatic:RegenTimer"], out var ts)
+                ? ts : null;
+
+            #region app.UseStaticPageFallback()
+#if ENABLE_STATIC_PAGE_FALLBACK
+
+            if (allowSSG)
+            {
+                builder.Services.AddStaticPageFallback(
+                    cfg =>
+                    {
+                        cfg.AlwaysDefaultFile = false;
+                    });
+            }
+
+#endif
+            #endregion
+
 
             var app = builder.Build();
-            app.MapRazorPages();
-#if TBC
-            app.GenerateStaticContent("../../../../DPVreony.Website/wwwroot");
+
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseHttpsRedirection();
+
+            #region app.UseStaticPageFallback()
+#if ENABLE_STATIC_PAGE_FALLBACK
+
+            if (allowSSG)
+            {
+                app.UseStaticPageFallback();
+            }
+
 #endif
+            #endregion
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            if (allowSSG)
+            {
+                app.GenerateStaticContent(
+                    app.Environment.WebRootPath,
+                    exitWhenDone: exitWhenDone,
+                    alwaysDefaultFile: false,
+                    dontUpdateLinks: false,
+                    dontOptimizeContent: false,
+                    regenerationInterval: regenInterval);
+            }
+
             await app.RunAsync()
                 .ConfigureAwait(false);
 
             return 0;
+        }
+
+        private static StaticResourcesInfoProvider GetStaticResourcesInfoProvider(WebApplicationBuilder builder)
+        {
+            var environment = builder.Environment;
+
+            return new StaticResourcesInfoProvider()
+                .Add(StaticPagesFactory.GetCollection())
+                .Add(environment.GetWebRootCssResources(
+                    ["css/**/*.css"]))
+                .Add(environment.GetWebRootCssResources(
+                    ["lib/bootstrap/**/*.css"],
+                    dontOptimize: true))
+                .Add(environment.GetWebRootJsResources(
+                    ["js/**/*.js"]))
+                .Add(environment.GetWebRootJsResources(
+                    ["lib/bootstrap/**/*.js",
+                        "lib/bootstrap/**/*.js.map",
+                        "lib/jquery*/**/*.js",
+                        "lib/jquery*/**/*.map"],
+                    dontOptimize: true))
+                .Add(environment.GetWebRootBinResources(
+                    ["favicon.ico"]))
+                .Add(environment.GetWebRootBinResources(
+                    ["lib/bootstrap/license",
+                        "lib/jquery*/license.*"],
+                    dontOptimize: true));
         }
     }
 }
---
+++ src/DPVreony.WebsiteBuilder/StaticPagesFactory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using AspNetStatic;
+
+namespace DPVreony.WebsiteBuilder
+{
+    public class StaticPagesFactory
+    {
+        public static IEnumerable<PageResource> GetCollection() =>
+        [
+            new("/"),
+            new("/Privacy"),
+            new("/Blog/Articles/"),
+        ];
+    }
+}
