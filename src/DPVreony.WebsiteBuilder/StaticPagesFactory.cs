ab794bee0816faee4561733c75557942e760fba0
David Vreony <dpvreony@users.noreply.github.com>
2026-03-27 09:51:45 +0000

    Update StaticPagesFactory.cs

--- src/DPVreony.WebsiteBuilder/StaticPagesFactory.cs
+++ src/DPVreony.WebsiteBuilder/StaticPagesFactory.cs
@@ -6,15 +6,33 @@ namespace DPVreony.WebsiteBuilder
 {
     public class StaticPagesFactory
     {
-        public static IEnumerable<PageResource> GetCollection() =>
-        [
-            new("/"),
-            new("/opensource/"),
-            new("/resume/"),
-            new("/privacy/"),
-            new("/articles/"),
-            new("/articles/designing-vetuviem/"),
-        ];
+        public static IEnumerable<PageResource> GetCollection()
+        {
+            yield return new("/");
+            yield return new("/opensource/");
+            yield return new("/resume/");
+            yield return new("/privacy/");
+            yield return new("/articles/");
+            yield return new("/articles/architecture-decision-records/");
+            yield return new("/articles/configuring-android-with-exchange-alias/");
+            yield return new("/articles/crawling-a-website-as-integration-test/");
+            yield return new("/articles/designing-apm-splat/");
+            yield return new("/articles/designing-nucleotide/");
+            yield return new("/articles/designing-vetuviem/");
+            yield return new("/articles/designing-whipstaff/");
+            yield return new("/articles/docfx-as-a-downstream-repository/");
+            yield return new("/articles/feature-usage-tracking/");
+            yield return new("/articles/knowledge-management-timeline/");
+            yield return new("/articles/mermaid-with-statiq/");
+            yield return new("/articles/mitigate-registration-page-email-abuse/");
+            yield return new("/articles/mitigating-j-curves/");
+            yield return new("/articles/naming-convention-net-projects/");
+            yield return new("/articles/opensource/");
+            yield return new("/articles/running-a-software-developer-interview-process/");
+            yield return new("/articles/sky-using-own-modem-and-router/");
+            yield return new("/articles/sourcegenerator-using-json/");
+            yield return new("/articles/using-technical-steering/");
+        }
 
         public static IEnumerable<BinResource> GetBinResources() =>
         [
